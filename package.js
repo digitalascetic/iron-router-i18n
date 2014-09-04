cedb7c91e168629a65814222e9b742d67b3dde9f
Martino Piccinato <mp@yoolab.com>
2014-09-04 08:01:14 +0200

    Upgrade to 0.9 package system

--- package.js
+++ package.js
@@ -1,14 +1,18 @@
 Package.describe({
     summary: 'Iron router support for i18n routing prefix',
-    version: '0.1'
+    version: "0.1.1",
+    git: "https://github.com/yoolab/iron-router-i18n.git",
+    version: '0.1.1'
 });
 
 Package.on_use(function (api) {
 
+    api.versionsFrom("METEOR@0.9.0");
+
     api.use('reactive-dict', ['client', 'server']);
     api.use('deps', ['client', 'server']);
     api.use('underscore', ['client', 'server']);
-    api.use('iron-router', ['client', 'server']);
+    api.use("iron:router@0.9.1", ['client', 'server']);
 
 
     api.add_files('lib/router.js', ['client', 'server']);
@@ -19,8 +23,8 @@ Package.on_use(function (api) {
 
 Package.on_test(function (api) {
 
-    api.use('iron-router', ['client', 'server']);
-    api.use('iron-router-i18n', ['client', 'server']);
+    api.use('iron:router', ['client', 'server']);
+    api.use("martino:iron-router-i18n", ['client', 'server']);
     api.use('tinytest', ['client', 'server']);
     api.use('test-helpers', ['client', 'server']);
     api.use('reactive-dict', ['client', 'server']);
